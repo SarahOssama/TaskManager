35394a47475dd554b024f71276a18ee495243da7
SarahOssama <62894826+SarahOssama@users.noreply.github.com>
2021-04-23 16:12:31 +0200

    before mock

--- src/App.js
+++ src/App.js
@@ -1,15 +1,75 @@
 //import React from 'react'
-import Header from './Components/Header'
+import { useState, useEffect } from "react";
+import Header from "./Components/Header";
+import Tasks from "./Components/Tasks";
+import AddTask from "./Components/AddTask";
 
+const App = () => {
+  const [showAddTask, setShowAddTask] = useState(false);
 
-function App() {
-  const name ='Sarah'
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    const getTasks = async () => {
+      const tasksFromServer = await fetchTasks();
+      setTasks(tasksFromServer);
+    };
+    getTasks();
+  }, []);
+
+  //Fetch Tasks
+  const fetchTasks = async () => {
+    const res = await fetch("http://localhost:5000/tasks");
+    const data = await res.json();
+
+    return data;
+  };
+  //Add Task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 100000) + 1;
+    console.log(id);
+
+    const newTask = { id, ...task };
+    setTasks([...tasks, newTask]);
+  };
+
+  //Delete Task
+  const deleteTask = async (id) => {
+    await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: "Delete",
+    });
+
+    // console.log('delete',id)
+    setTasks(tasks.filter((task) => task.id !== id)); //filter the tasks to show only the tasks I want (delete unwanted task)
+  };
+
+  //Toggle Reminder
+  const toggleReminder = (id) => {
+    // console.log(id);
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    );
+  };
+
   return (
     <div className="container">
-      <Header />
+      {" "}
+      <Header
+        onAdd={() => setShowAddTask(!showAddTask)}
+        showAdd={showAddTask}
+        // showadd is a prop
+      />
+      {showAddTask && <AddTask onAdd={addTask} />}
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
+      ) : (
+        "No Tasks are available"
+      )}
     </div>
   );
-}
+};
 // class App extends React.Component{
 //   render(){
 //     return <h1>Hello from React</h1>
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,25 +1,34 @@
-import PropTypes from 'prop-types'
+import PropTypes from "prop-types";
+import Button from "./Button";
 
-
-
-const Header = ({title}) => {
-    return (
-        <header className='header'>
-            <h1>{title}</h1>
-            <button className='btn'>Add</button>
-        </header>
-    )
-}
+const Header = ({ title, onAdd, showAdd }) => {
+  const onClick = () => {
+    // fetch('https://jsonplaceholder.typicode.com/todos/1')
+    // .then(response => response.json())
+    // .then(json => console.log(json))
+    //console.log("click");
+  };
+  return (
+    <header className="header">
+      <h1>{title}</h1>
+      <Button
+        color={showAdd ? "green" : "red"}
+        text={showAdd ? "Close" : "Add"}
+        onClick={onAdd}
+      />
+    </header>
+  );
+};
 
 Header.defaultProps = {
-    title : 'Task Tracker',
-}
+  title: "Task Tracker",
+};
 
-Header.propTypes ={
-    title: PropTypes.string.isRequired,
-}
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
+};
 
-export default Header
+export default Header;
 
 // const Header = (props) => {
 //     return (
